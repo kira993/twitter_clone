971d000f6fe02bcdcbfdfb609b6a4840b837572d
kira993 <lanmi_93@live.com>
2016-05-31 08:47:40 +0200

    Polished GUI a bit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,27 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :posts, dependent: :destroy # remove a user's posts if his account is deleted.
-                  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-                  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+         has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+         has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-                  has_many :following, through: :active_relationships, source: :followed
-                  has_many :followers, through: :passive_relationships, source: :follower
+         has_many :following, through: :active_relationships, source: :followed
+         has_many :followers, through: :passive_relationships, source: :follower
 
 
-                  # helper methods
+         # helper methods
 
-                  # follow another user
-                  def follow(other)
-                    active_relationships.create(followed_id: other.id)
-                  end
-
-                  # unfollow a user
-                  def unfollow(other)
-                    active_relationships.find_by(followed_id: other.id).destroy
-                  end
-
-                  # is following a user?
-                  def following?(other)
-                    following.include?(other)
-                  end
+         # follow another user
+         def follow(other)
+           active_relationships.create(followed_id: other.id)
          end
+
+         # unfollow a user
+         def unfollow(other)
+           active_relationships.find_by(followed_id: other.id).destroy
+         end
+
+         # is following a user?
+         def following?(other)
+           following.include?(other)
+         end
+end
